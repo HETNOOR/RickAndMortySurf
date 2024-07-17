5063399d1649fe3b9ab0ca4a31ce23a398854971
HETNOOR <daffixipop@gmail.com>
2024-07-17 18:07:18 +0700

    Initial Commit

---
+++ RikAndMortiSurf/RikAndMortiSurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  RikAndMortiSurfApp.swift
+//  RikAndMortiSurf
+//
+//  Created by Максим Герасимов on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RikAndMortiSurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
